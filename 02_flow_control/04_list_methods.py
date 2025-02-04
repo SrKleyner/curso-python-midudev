8f7156e191dd6648d39daf747cfcd5ead1254f66
SrKleyner <180644185+SrKleyner@users.noreply.github.com>
2025-02-03 21:32:22 -0400

    resolucion de ejercicios de listas con metodos

--- 02_flow_control/04_list_methods.py
+++ 02_flow_control/04_list_methods.py
@@ -81,25 +81,56 @@ print('🐹' in animals) # -> False
 # Inserta el número 10 en la posición 2 usando insert().
 # Modifica el primer elemento de la lista para que sea 0.
 
+list1 = [1, 2, 3, 4, 5]
+list1.append(6)
+list1.insert(2, 10)
+list1[0] = 0
+print(list1)
+
 # Ejercicio 2: Combinar y limpiar listas
 # Crea dos listas:
-# lista_a = [1, 2, 3]
-# lista_b = [4, 5, 6, 1, 2]
+lista_a = [1, 2, 3]
+lista_b = [4, 5, 6, 1, 2]
 # Extiende lista_a con lista_b usando extend().
 # Elimina la primera aparición del número 1 en lista_a usando remove().
 # Elimina el elemento en el índice 3 de lista_a usando pop(). Imprime el elemento eliminado.
 # Limpia completamente lista_b usando clear().
+
+lista_a.extend(lista_b)
+print('extend = ', lista_a)
+lista_a.remove(1)
+print('remove = ', lista_a)
+popvar = lista_a.pop(3)
+print('pop = ', popvar)
+print(lista_a)
+lista_a.clear()
+print('lista limpia = ', lista_a)
 
 # Ejercicio 3: Slicing y eliminación con del
 # Crea una lista con los números del 1 al 10.
 # Utiliza slicing y del para eliminar los elementos desde el índice 2 hasta el 5 (sin incluir el 5).
 # Imprime la lista resultante.
 
+listanueva = [1, 2, 3 , 4, 5, 6, 7, 8, 9, 10]
+del listanueva[2:5]
+print(listanueva)
+
 # Ejercicio 4: Ordenar y contar
 # Crea una lista con los siguientes números: [5, 2, 8, 1, 9, 4, 2].
 # Ordena la lista de forma ascendente usando sort().
 # Cuenta cuántas veces aparece el número 2 en la lista usando count().
 # Comprueba si el número 7 está en la lista usando in.
+
+numeros = [5, 2, 8, 1, 9, 4, 2]
+print(f'\n-------------------------------------\n')
+print(f'Lista desordenada: {numeros}')
+numeros.sort()
+cuantity_of_two = numeros.count(2)
+print(f'Lista ordenada: {numeros}')
+print(f'Cantidad de dos: {cuantity_of_two}')
+print(f'Existe el numero 7 ?: {7 in numeros} \n')
+print(f'-------------------------------------\n')
+
 
 # Ejercicio 5: Copia vs. Referencia
 # Crea una lista llamada original con los números [1, 2, 3].
@@ -109,6 +140,15 @@ print('🐹' in animals) # -> False
 # Modifica el primer elemento de la lista referencia a 10.
 # Imprime las cuatro listas (original, copia_1, copia_2, referencia) y observa los cambios.
 
+original = [1, 2, 3]
+copia_1 = original[:]
+copia_2 = original.copy()
+
+
 # Ejercicio 6: Ordenar strings sin diferenciar mayúsculas y minúsculas.
 # Crea una lista con las siguientes cadenas: ["Manzana", "pera", "BANANA", "naranja"].
 # Ordena la lista sin diferenciar entre mayúsculas y minúsculas.
+
+unsorted_list = ["Manzana", "pera", "BANANA", "naranja"]
+unsorted_list.sort(key=str.lower)
+print(f'lista ordenada: {unsorted_list}')
